9ffef8a5e0fce3dcd58ffaf43e88956dd21df845
kwstreet <mm+git@blat.ca>
2022-05-23 18:09:01 -0400

    Render div

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>This was a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
